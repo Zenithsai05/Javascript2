293937f2490c1848e0d36d8b63283aab50d09fa8
Zenithsai05 <silverbackattack10@gmail.com>
2024-12-17 06:09:30 +0000

    arrow commit

---
+++ basics3/3arrows.js
@@ -0,0 +1,29 @@
+const user = {
+    username: "zenith",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username} , Welcome To Website`);
+        console.log(this);
+        
+    }
+}
+
+//user.welcomeMessage()
+//user.username = "sai"
+//user.welcomeMessage()
+
+// console.log(this); // it will show empty because ther is no value 
+
+// Arrow function
+const chai = () => {  // => this is arrow function
+    let username = "sai"
+    console.log(this);
+    
+}
+// chai ()
+
+const addnum = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addnum(3, 8));
